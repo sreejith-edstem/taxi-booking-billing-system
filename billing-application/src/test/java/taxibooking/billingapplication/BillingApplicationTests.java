c09e1c1475b277464e68754eb5743c4c88cf449a
sreejith-edstem <sreejith.s@edstem.com>
2024-02-01 14:18:34 +0530

    initialize-project

---
+++ billing-application/src/test/java/taxibooking/billingapplication/BillingApplicationTests.java
@@ -0,0 +1,13 @@
+package taxibooking.billingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
