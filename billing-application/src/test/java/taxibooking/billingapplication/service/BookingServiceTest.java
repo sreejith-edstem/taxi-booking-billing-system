7305be44f76ce2ba04a476710b1f9e1668ac3071
sreejith-edstem <sreejith.s@edstem.com>
2024-02-02 13:00:36 +0530

    removed-unwanted-commands

--- billing-application/src/test/java/taxibooking/billingapplication/service/BookingServiceTest.java
+++ billing-application/src/test/java/taxibooking/billingapplication/service/BookingServiceTest.java
@@ -1,41 +1,28 @@
 package taxibooking.billingapplication.service;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
-import taxibooking.billingapplication.contract.request.BookingRequest;
-import taxibooking.billingapplication.contract.response.BookingResponse;
-import taxibooking.billingapplication.model.Booking;
 import taxibooking.billingapplication.repository.BookingRepository;
 import taxibooking.billingapplication.repository.TaxiRepository;
 import taxibooking.billingapplication.repository.UserRepository;
-
-import static org.mockito.Mockito.when;
 
 public class BookingServiceTest {
     private BookingRepository bookingRepository;
     private UserRepository userRepository;
     private TaxiRepository taxiRepository;
     private ModelMapper modelMapper;
-    private BookingService bookingService = new BookingService(null,null,null,null);
+    private BookingService bookingService = new BookingService(null, null, null, null);
+
     @BeforeEach
-    public void init(){
+    public void init() {
         MockitoAnnotations.openMocks(this);
         bookingRepository = Mockito.mock(BookingRepository.class);
         userRepository = Mockito.mock(UserRepository.class);
         taxiRepository = Mockito.mock(TaxiRepository.class);
         modelMapper = Mockito.mock(ModelMapper.class);
-        bookingService = new BookingService(bookingRepository,userRepository,taxiRepository,modelMapper);
+        bookingService = new BookingService(bookingRepository, userRepository, taxiRepository, modelMapper);
     }
-//    @Test
-//    void createBookingTest(){
-//        Booking booking = new Booking();
-//        BookingRequest request = new BookingRequest();
-//        BookingResponse response = new BookingResponse();
-//        when(modelMapper.map(request, Booking.class)).thenReturn(booking);
-//        when(bookingRepository.save())
-//    }
 
 }
