bd26e150f7d66221a738196c40e15aa615753e67
sreejith-edstem <sreejith.s@edstem.com>
2024-02-02 12:45:38 +0530

    postman-collection-updated

--- billing-application/src/test/java/taxibooking/billingapplication/service/BookingServiceTest.java
+++ billing-application/src/test/java/taxibooking/billingapplication/service/BookingServiceTest.java
@@ -5,9 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
+import taxibooking.billingapplication.contract.request.BookingRequest;
+import taxibooking.billingapplication.contract.response.BookingResponse;
+import taxibooking.billingapplication.model.Booking;
 import taxibooking.billingapplication.repository.BookingRepository;
 import taxibooking.billingapplication.repository.TaxiRepository;
 import taxibooking.billingapplication.repository.UserRepository;
+
+import static org.mockito.Mockito.when;
 
 public class BookingServiceTest {
     private BookingRepository bookingRepository;
@@ -24,9 +29,13 @@ public class BookingServiceTest {
         modelMapper = Mockito.mock(ModelMapper.class);
         bookingService = new BookingService(bookingRepository,userRepository,taxiRepository,modelMapper);
     }
-    @Test
-    void createBookingTest(){
-
-    }
+//    @Test
+//    void createBookingTest(){
+//        Booking booking = new Booking();
+//        BookingRequest request = new BookingRequest();
+//        BookingResponse response = new BookingResponse();
+//        when(modelMapper.map(request, Booking.class)).thenReturn(booking);
+//        when(bookingRepository.save())
+//    }
 
 }
--- billing-application/src/test/java/taxibooking/billingapplication/service/TaxiServiceTest.java
+++ billing-application/src/test/java/taxibooking/billingapplication/service/TaxiServiceTest.java
@@ -1,12 +1,16 @@
 package taxibooking.billingapplication.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
-import taxibooking.billingapplication.repository.BookingRepository;
+import taxibooking.billingapplication.contract.request.TaxiRequest;
+import taxibooking.billingapplication.model.Taxi;
 import taxibooking.billingapplication.repository.TaxiRepository;
-import taxibooking.billingapplication.repository.UserRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class TaxiServiceTest {
     private TaxiRepository taxiRepository;
@@ -18,5 +22,14 @@ public class TaxiServiceTest {
         taxiRepository = Mockito.mock(TaxiRepository.class);
         modelMapper = Mockito.mock(ModelMapper.class);
         taxiService = new TaxiService(taxiRepository,modelMapper);
+    }
+    @Test
+    void testAddTaxi(){
+        Taxi taxi = new Taxi();
+        TaxiRequest request = new TaxiRequest();
+        when(modelMapper.map(request, Taxi.class)).thenReturn(taxi);
+        when(taxiRepository.save(taxi)).thenReturn(taxi);
+        Long result = taxiService.createTaxi(request);
+        assertEquals(taxi,result);
     }
 }
