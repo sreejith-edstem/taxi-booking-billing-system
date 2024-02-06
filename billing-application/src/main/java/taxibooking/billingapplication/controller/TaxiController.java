f33aedad6b30ec71e6c70300f45ea3673deecc99
sreejith-edstem <sreejith.s@edstem.com>
2024-02-06 11:31:22 +0530

    code-updated

--- billing-application/src/main/java/taxibooking/billingapplication/controller/TaxiController.java
+++ billing-application/src/main/java/taxibooking/billingapplication/controller/TaxiController.java
@@ -1,5 +1,6 @@
 package taxibooking.billingapplication.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +16,7 @@ public class TaxiController {
     private final TaxiService taxiService;
 
     @PostMapping("/taxi/create")
-    public Long addTaxi(@RequestBody TaxiRequest request) {
+    public Long addTaxi(@Valid @RequestBody TaxiRequest request) {
         return taxiService.addTaxi(request);
     }
 }
